10d0f10f8b6cd16441da8c9110e3b27245f897d5
jaa127 <jaa@sn127.fi>
2017-11-12 23:09:00 +0200

    tep-1004: perf tests
    performance tests for register report:
     - txt -only
     - json -only
     - txt and json
    
    Signed-off-by: jaa127 <jaa@sn127.fi>

--- perf/bin/sample-run.sh
+++ perf/bin/sample-run.sh
@@ -3,20 +3,21 @@
 exe_path=$1
 trg=$2
 
-storage=git
-
- 
-# --input.txn.glob "**.txn" \
-# --input.txn.dir data/perf-$trg/ \
+storage=fs
+# --input.git.ref $trg \
 
 (
 for i in 1 2 3 4 5; do 
 	time java -jar "$exe_path" \
 	--cfg perf-$storage.conf \
-	--input.git.ref $trg \
-	--output out/perf-$storage-$trg
+	--input.fs.glob "**.txn" \
+	--input.fs.dir data/perf-$trg/ \
+	--output out/perf-$storage-$trg \
+	--reporting.formats json txt \
+	--reporting.console false \
+	--reporting.reports register
 
 	echo
 done
-) > results/hw01/perf-$storage-$trg.txt  2>&1 
+) > results/hw01/0.7.0-next-perf-$storage-$trg-register_json_txt.txt  2>&1 
 
