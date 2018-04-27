19007ea1a8f46c28348c411626552d08c0cfe550
jaa127 <jaa@sn127.fi>
2018-04-27 23:31:30 +0300

    Update deps and build tools.
    Especially this updates sbt-coveralls, which in turn
    should fix aggregated coverage status and also source upload
    for cross-projects. Yay!
    
    See:
     * https://github.com/scoverage/sbt-coveralls/commit/40b0cbd4f10449e677c1982aca19fbbf90504061
     * https://github.com/scoverage/sbt-coveralls/issues/104
    
    Signed-off-by: jaa127 <jaa@sn127.fi>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,13 +22,13 @@ object Dependencies {
    * Versions
    */
   val betterFilesVersion = "3.4.0"
-  val catsVersion = "1.0.1"
-  val circeVersion = "0.9.1"
-  val scalatestVersion = "3.0.4"
-  val scallopVersion = "3.1.1"
-  val configVersion = "1.3.2"
+  val catsVersion = "1.1.0"
+  val circeVersion = "0.9.3"
+  val scalatestVersion = "3.0.5"
+  val scallopVersion = "3.1.2"
+  val configVersion = "1.3.3"
   val dirsuiteVersion = "0.7.0"
-  val jgitVersion = "4.10.0.201712302008-r"
+  val jgitVersion = "4.11.0.201803080745-r"
   val logbackVersion = "1.2.3"
   val scalaArmVersion = "2.0"
 
